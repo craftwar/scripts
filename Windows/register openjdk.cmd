4005161a5ae548fb24332079dbf2de06be772f8c
craftwar <craftwar@users.noreply.github.com>
2017-12-01 23:16:57 +0800

    fix register openjdk

--- Windows/register openjdk.cmd
+++ Windows/register openjdk.cmd
@@ -1,2 +1,6 @@
+::right click run as administrator
 assoc .jar=jar_auto_file
-ftype jar_auto_file="%JAVA_HOME%\bin\javaw.exe" -jar "%1" %*
+ftype jar_auto_file="%JAVA_HOME%\bin\javaw.exe" -jar "%%1" %%*
+
+:: original command run in cmd
+:: ftype jar_auto_file="%JAVA_HOME%\bin\javaw.exe" -jar "%1" %*

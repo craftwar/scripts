cc054dc6753f2fda3572a4f077efbdf2a77b1b61
craftwar <craftwar@users.noreply.github.com>
2018-01-13 14:35:02 +0800

    update OBS.cmd: Use variable

--- Windows/update OBS.cmd
+++ Windows/update OBS.cmd
@@ -1,15 +1,19 @@
 @rem 7zip doesn't support extract some formats (7z...etc) from pipe yet
 @rem curl -skL https://zlib.net/zlib1211.zip -f --retry 5 -C - | "C:\Program Files\7-Zip\7z.exe" x -si
+set file_url=https://ci.appveyor.com/api/projects/craftwar_appveyor/obs-studio/artifacts/OBS-git-craftwar.7z?branch=master
+set file=OBS-git-craftwar.7z
 
-if exist OBS-git-craftwar.7z (
-	curl -kLo OBS-git-craftwar-new.7z https://ci.appveyor.com/api/projects/craftwar_appveyor/obs-studio/artifacts/OBS-git-craftwar.7z?branch=master -f --retry 5 -z OBS-git-craftwar.7z
-    if exist OBS-git-craftwar-new.7z (
-        7z.exe x OBS-git-craftwar-new.7z -y -o.
-        move /y OBS-git-craftwar-new.7z OBS-git-craftwar.7z
+echo %file_url%
+
+if exist %file% (
+	curl -kLo %file%-new %file_url% -f --retry 5 -z %file%
+    if exist %file%-new (
+        7z.exe x %file%-new -y -o.
+        move /y %file%-new %file%
     )
 ) else (
-	curl -kLo OBS-git-craftwar.7z https://ci.appveyor.com/api/projects/craftwar_appveyor/obs-studio/artifacts/OBS-git-craftwar.7z?branch=master -f --retry 5 -C -
-	7z.exe x OBS-git-craftwar.7z -y -o.
+	curl -kLo %file% %file_url% -f --retry 5 -C -
+	7z.exe x %file% -y -o.
 )
 pause
 

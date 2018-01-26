481a2c6f4df9027fa8caa58477031739e987185b
craftwar <craftwar@users.noreply.github.com>
2018-01-27 01:49:19 +0800

    update OBS.cmd: Clean up and leave comment for setlocal

--- Windows/update OBS.cmd
+++ Windows/update OBS.cmd
@@ -1,9 +1,10 @@
 @rem 7zip doesn't support extract some formats (7z...etc) from pipe yet
 @rem curl -skL https://zlib.net/zlib1211.zip -f --retry 5 -C - | "C:\Program Files\7-Zip\7z.exe" x -si
+
+@rem setlocal
 set file_url=https://ci.appveyor.com/api/projects/craftwar_appveyor/obs-studio/artifacts/OBS-git-craftwar.7z?branch=master
 set file=OBS-git-craftwar.7z
 
-echo %file_url%
 
 if exist %file% (
 	curl -kLo %file%-new %file_url% -f --retry 5 -z %file%
@@ -15,6 +16,7 @@ if exist %file% (
 	curl -kLo %file% %file_url% -f --retry 5 -C -
 	7z.exe x %file% -y -o.
 )
+@rem endlocal
 pause
 
 :: old force update

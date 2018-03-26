e96f25be854e0c533b593b3716c869fb2b225c9d
craftwar <craftwar@users.noreply.github.com>
2018-03-27 00:59:28 +0800

    update KVIrc.cmd: Exclude more files

--- Windows/update KVIrc.cmd
+++ Windows/update KVIrc.cmd
@@ -1,3 +1,3 @@
-"C:\Program Files\7-Zip\7z.exe" x D:\downloads\KVIrc*.exe -y -oD:\KVIrc -x!$PLUGINSDIR -x!vcredist_x64.exe
+"C:\Program Files\7-Zip\7z.exe" x D:\downloads\KVIrc*.exe -y -oD:\KVIrc -x!$PLUGINSDIR -x!vcredist_x64.exe -x!vc_redist.x64.exe -x!msvcr120.dll
 pause
 del /Q D:\downloads\KVIrc*.exe

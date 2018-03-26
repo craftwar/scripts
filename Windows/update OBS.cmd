d95ac4628ae761fc27ae91b342c0af173b2cef53
craftwar <craftwar@users.noreply.github.com>
2018-03-27 00:48:28 +0800

    update OBS.cmd: Use variable to set 7z.exe path

--- Windows/update OBS.cmd
+++ Windows/update OBS.cmd
@@ -4,17 +4,20 @@
 @rem setlocal
 set file_url=https://ci.appveyor.com/api/projects/craftwar_appveyor/obs-studio/artifacts/OBS-git-craftwar.7z?branch=master
 set file=OBS-git-craftwar.7z
+set file-new=%file%-new
+set _7z=7z.exe
+::set _7z_options=
 
 
 if exist %file% (
-	curl -kLo %file%-new %file_url% -f --retry 5 -z %file%
+	curl -kLo %file-new% %file_url% -f --retry 5 -z %file%
     if exist %file%-new (
-        7z.exe x %file%-new -y -o.
-        move /y %file%-new %file%
+        %_7z% x %file-new% -y -o. %_7z_options%
+        move /y %file-new% %file%
     )
 ) else (
 	curl -kLo %file% %file_url% -f --retry 5 -C -
-	7z.exe x %file% -y -o.
+	%_7z% x %file% -y -o. %_7z_options%
 )
 @rem endlocal
 pause

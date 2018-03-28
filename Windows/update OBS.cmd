d642a219880dbe36792a4e80878a44ec73a9b34b
craftwar <craftwar@users.noreply.github.com>
2018-03-28 14:50:30 +0800

    update OBS.cmd: Add curl errorlevel check

--- Windows/update OBS.cmd
+++ Windows/update OBS.cmd
@@ -2,23 +2,29 @@
 @rem curl -skL https://zlib.net/zlib1211.zip -f --retry 5 -C - | "C:\Program Files\7-Zip\7z.exe" x -si
 
 @rem setlocal
+@echo off
 set file_url=https://ci.appveyor.com/api/projects/craftwar_appveyor/obs-studio/artifacts/OBS-git-craftwar.7z?branch=master
 set file=OBS-git-craftwar.7z
 set file-new=%file%-new
-set _7z=7z.exe
+set _7z=7za.exe
 ::set _7z_options=
 
 
 if exist %file% (
 	curl -kLo %file-new% %file_url% -f --retry 5 -z %file%
-    if exist %file%-new (
-        %_7z% x %file-new% -y -o. %_7z_options%
-        move /y %file-new% %file%
-    )
+	if %ERRORLEVEL% EQU 0 (
+		if exist %file%-new (
+			move /y %file-new% %file%
+			%_7z% x %file% -y -o. %_7z_options%
+		)
+	)
 ) else (
 	curl -kLo %file% %file_url% -f --retry 5 -C -
-	%_7z% x %file% -y -o. %_7z_options%
+	if %ERRORLEVEL% EQU 0 (
+		%_7z% x %file% -y -o. %_7z_options%
+	)
 )
+if %ERRORLEVEL% NEQ 0 (echo error!)
 @rem endlocal
 pause
 

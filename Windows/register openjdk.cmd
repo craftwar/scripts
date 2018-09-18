330baf72f8d2b25a092b31af993f7625395d42ae
craftwar <craftwar@users.noreply.github.com>
2018-09-18 08:38:05 +0800

    Update "register openjdk.cmd"

--- Windows/register openjdk.cmd
+++ Windows/register openjdk.cmd
@@ -1,6 +1,6 @@
 ::right click run as administrator
-assoc .jar=jar_auto_file
-ftype jar_auto_file="%JAVA_HOME%\bin\javaw.exe" -jar "%%1" %%*
+assoc .jar=jarfile
+ftype jarfile="%JAVA_HOME%\bin\javaw.exe" -jar "%%1" %%*
 
-:: original command run in cmd
-:: ftype jar_auto_file="%JAVA_HOME%\bin\javaw.exe" -jar "%1" %*
+:: Or run this in cmd as administrator manually
+:: ftype jarfile="%JAVA_HOME%\bin\javaw.exe" -jar "%1" %*

f578c983c3b3cd66c1792b24e42ea4bf3bf9556c
craftwar <craftwar@users.noreply.github.com>
2018-12-21 11:58:32 +0800

    Use variable, easier to update Java version When Java is updated, only JAVA_HOME variable is required to be changed.

--- Windows/register openjdk.cmd
+++ Windows/register openjdk.cmd
@@ -1,6 +1,6 @@
 ::right click run as administrator
 assoc .jar=jarfile
-ftype jarfile="%JAVA_HOME%\bin\javaw.exe" -jar "%%1" %%*
+ftype jarfile="%%JAVA_HOME%%\bin\javaw.exe" -jar "%%1" %%*
 
 :: Or run this in cmd as administrator manually
 :: ftype jarfile="%JAVA_HOME%\bin\javaw.exe" -jar "%1" %*
